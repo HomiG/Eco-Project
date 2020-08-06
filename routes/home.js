a0eb14bc9cc05bac14df1d1f6d4a52e220a43815
Homi <44709877+HomiG@users.noreply.github.com>
2020-08-07 00:30:01 +0300

    encrypt Password -> Ready For Database

--- routes/home.js
+++ routes/home.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser')
+const bcrypt = require('bcrypt')
 const router = express.Router();
 
 router.use(express.json());
@@ -12,10 +13,21 @@ router.get('/', function (req, res) {
   });
 
 
-router.post('/', function(req, res){
-  var v = {username: req.body.username, pa: req.body.password}
+  //accepts the username and the password from the user, with the POST method.
+  //encrypts the password, and sends it back to the user.
+router.post('/', async function(req, res){
+  try{
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(req.body.password, salt)
+  var v = {username: req.body.username, pa: hashedPassword}
   res.status(201).send(v);
+  }
+  catch{
+    res.status(500)
+  }
 });
 
+// //check if given password maches saved password
+// if(await bcrypt.compare(req.body.password, savedPassword));
 
 module.exports = router;
