a8921c9278ee96d2e7404ec302a8ce6a5537483f
Homi <44709877+HomiG@users.noreply.github.com>
2020-08-06 20:28:32 +0300

    udpate

--- routes/home.js
+++ routes/home.js
@@ -1,8 +1,20 @@
 const express = require('express');
+const bodyParser = require('body-parser')
+
+
 const router = express.Router();
+
+router.use(bodyParser());
+
+
 
 router.get('/', function (req, res) {
     res.render('../views/index.ejs')
   });
+
+  app.post('/', function(req, response){
+
+  });
+
 
 module.exports = router;
