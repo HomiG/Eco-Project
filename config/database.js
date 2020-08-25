a89704e31e86c2f0f92b3dc46a2e1dd3de1a4574
Homi <44709877+HomiG@users.noreply.github.com>
2020-08-25 17:03:25 +0300

    Show Heatmap on Command

--- config/database.js
+++ config/database.js
@@ -5,7 +5,7 @@ var connection  = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: '',
-  database: 'ecoproject1',
+  database: 'ecoproject',
   port: 3306
 });
 
--- public/js/leafletHeatmap.js
+++ public/js/leafletHeatmap.js
@@ -3,18 +3,18 @@
 
 // Create Heatmap Instance
 var heatmapLayer = new HeatmapOverlay(cfg);
-
+var heatmapData;
 
 // Declaer the baseLayer
 baseLayer = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoicmFuZG9tbmFtZWdyIiwiYSI6ImNrZHJwaWdkbDBiNXYyeW83NmQ1aDU2bDkifQ.TAMboxLmOiUsmRbljrplEQ',
- {
+  {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     id: 'mapbox/streets-v11',
     tileSize: 512,
     zoomOffset: -1,
     accessToken: 'your.mapbox.access.token'
-})
+  })
 
 
 
@@ -26,27 +26,41 @@ var mymap = L.map('mapid', {
 });
 
 
-heatmapLayer.setData(testData);
+heatmapData = async function(){
+  $.ajax({
+    url: "/getHeatmap",
+    type: "POST",
+    cache: false,
+    contentType: false,
+    processData: false,
+    data: formSensitiveAreas,
+    success: function (response) {
+      statusDiv.innerHTML = 'File Uploaded Succesfully!'
+      alert("Passed Into Database")
+      //console.log(response);
+    }
+  })
+
+
+}
+heatmapLayer.setData();
+
+
 
 
 // Add ta marker in Given Cetner Of Patras 
-var marker = L.marker(patrasLatLng).addTo(mymap); 
+var marker = L.marker(patrasLatLng).addTo(mymap);
 
 //Patras Circle Range
 var circle = L.circle(patrasLatLng, {
-    color: 'green',
-    fillColor: '#3388ff',
-    fillOpacity: 0.01   ,
-    radius: 10000 //10KM
+  color: 'green',
+  fillColor: '#3388ff',
+  fillOpacity: 0.01,
+  radius: 10000 //10KM
 }).addTo(mymap);
 
 
 
 
 
-var but = document.getElementById('button1')
-
-but.addEventListener('click', event => {
-  heatmapLayer.addData({lat:38.249995780478734, lng: 21.73734389646722, count: 5});
-});
-
+var but = document.getElementById('but')
