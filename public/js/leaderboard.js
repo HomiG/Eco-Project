6b466b7756af541c14f5c94e6c0e29dc4dee2213
Homi <44709877+HomiG@users.noreply.github.com>
2020-08-30 20:13:57 +0300

    CONFIDENCE

--- public/js/leaderboard.js
+++ public/js/leaderboard.js
@@ -3,3 +3,9 @@ let lastMonthTimestamp = {
     end:   1575158400
 }
 
+connection.query("SELECT SUM(confidence) as walking FROM entry "+
+"INNER JOIN locationconnectactivity on entry.entryId=locationconnectactivity.entryId "+
+"INNER JOIN activity1 on activity1.aa1=locationconnectactivity.a1 "+
+"INNER JOIN activity1connectactivity2 on activity1.aa1=activity1connectactivity2.a1 "+
+"INNER JOIN activity2 on activity2.aa2=activity1connectactivity2.a2  "+
+"WHERE type= 'ON_FEET' OR type='ON_BICYCLE' AND entry.userId='")
