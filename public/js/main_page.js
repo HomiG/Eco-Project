41b6ad03d747e2a4b8c96a699505d6c9e59219f0
DimitraKalam <demi.kalam@gmail.com>
2020-09-17 19:13:52 +0300

    frontend stuff

--- public/js/main_page.js
+++ public/js/main_page.js
@@ -144,8 +144,3 @@ function myFunction() {
 }
 //
 
-
-function scrollToTop() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
